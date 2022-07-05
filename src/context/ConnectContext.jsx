9fb1a3945dccb163443526419120ef887b258e23
dayolovesruby <dayoadewuyi@protonmail.com>
2022-07-05 23:51:20 +0100

    tr

--- src/context/ConnectContext.jsx
+++ src/context/ConnectContext.jsx
@@ -307,7 +307,7 @@ export const ConnectProvider = ({ children }) =>{
  
   const uauthOptions = {
     clientID: '68ee4d6b-dabe-48e1-a6ed-9f7b907eca13',
-    redirectUri: 'http://127.0.0.1',
+    redirectUri: 'https://ox-spence.vercel.app/',
   
     // Must include both the openid and wallet scopes.
     scope: 'openid wallet',
